f2dc34228750e7232aafaab6d4f27c11103e5306
SySon253 <162858665+SySon253@users.noreply.github.com>
2026-04-26 23:10:57 +0700

    update frame

--- src/main/java/vn/com/shop/controller/page/HomeController.java
+++ src/main/java/vn/com/shop/controller/page/HomeController.java
@@ -9,4 +9,16 @@ public class HomeController {
     public String home() {
         return "guest/index";
     }
+
+    @GetMapping(value = "/shop")
+    public String shop() { return "guest/shop"; }
+
+    @GetMapping(value = "/detail")
+    public String detail() { return "guest/detail"; }
+
+    @GetMapping(value = "/cart")
+    public String cart() { return "guest/cart"; }
+
+    @GetMapping(value = "/checkout")
+    public String checkout() { return "guest/checkout"; }
 }
